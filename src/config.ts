fe6323dafa5fda2b235a8ed6c99387d11b0fa3e2
agyarg <sibawaihagyar@gmail.com>
2024-07-30 19:12:58 +0700

    first commit

---
+++ src/config.ts
@@ -0,0 +1,78 @@
+export const socialLinks = [
+  {
+    name: "Mail",
+    href: "mailto:lifeofager@gmail.com",
+  },
+  {
+    name: "Twitter",
+    href: "https://twitter.com/kosewide",
+  },
+  {
+    name: "Github",
+    href: "https://github.com/agyarg",
+  },
+];
+
+export const projects = [
+  {
+    name: "HaveAniesDay",
+    href: "https://v1.haveaniesday.com",
+    image: "./showcase/haveaniesday.png",
+    desc: "A unique presidential campaign",
+    stacks: ["Next.js", "Tailwind CSS", "Headless UI", "Howler.js"],
+  },
+  {
+    name: "Sundafolk",
+    href: "https://sundafolk.pages.dev",
+    image: "./showcase/test.png",
+    desc: "Sundanese language learning material platform",
+    stacks: ["Svelte", "SvelteKit", "Typescript", "Tailwind CSS", "i18n"],
+  },
+  {
+    name: "HAD Times",
+    href: "https://hadtimes.com",
+    image: "./showcase/hadtimes.png",
+    desc: "A clothing brand with good concepts",
+    stacks: ["Vue", "Nuxt.js", "Tailwind CSS"],
+  },
+  {
+    name: "Simba Refinery",
+    href: "https://simbarefinery.com",
+    image: "./showcase/simbarefinery.png",
+    desc: "Business Profile",
+    stacks: [
+      "Vue",
+      "Nuxt.js",
+      "Tailwind CSS",
+      "Headless UI",
+      "Swiper.js",
+      "i18n",
+    ],
+  },
+  {
+    name: "Bella Belles",
+    href: "https://bellabelles.id",
+    image: "./showcase/test.png",
+    desc: "Business Profile",
+    stacks: [
+      "Svelte",
+      "SvelteKit",
+      "Tailwind CSS",
+      "Motion One",
+      "i18n",
+      "Ambla",
+    ],
+  },
+];
+
+export const tools = [
+  {
+    name: "Tech Stack",
+    content:
+      "React, Next.js, Svelte, SvelteKit, Vue, Nuxt, Astro, Tailwind CSS.",
+  },
+  {
+    name: "Code Editor",
+    content: "Visual Studio Code",
+  },
+];
